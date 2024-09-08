ce3bf6b80a7413b0254020e6088971343fbc9f40
Jamir16 <ja861910@ucf.edu>
2024-09-08 20:57:28 +0000

    Finished claculate distance

--- fracturing.c
+++ fracturing.c
@@ -1,19 +1,49 @@
 #include <stdio.h>
+#include <math.h>
 
-void calculateDistance ( int arg1 , int arg2 )
+double askForUserInput()
 {
-printf ("function") ;
+    double askForUser = 0;
+    scanf("%lf", &askForUser);
+    return askForUser;
 }
+
+
+double calculateDistance () {
+    double distance = 0;
+
+//     – Point #1 entered: x1 = -whatever was entered-; y1
+// = -whatever was entered-
+    double x1 = askForUserInput(); 
+    double y1 = askForUserInput();
+    printf("Point #1 entered: x1 = %f, ", x1);
+    printf("y1 = %f\n", y1);
+
+
+// – Point #2 entered: x2 = -whatever was entered-; y2
+// = -whatever was entered-
+    double x2 = askForUserInput();
+    double y2 = askForUserInput();
+    printf("Point #2 entered: x2 = %f, ", x2);
+    printf("y2 = %f\n", y2);
+
+
+// – The distance between the two points is -whatever is
+// computed-
+    distance = sqrt(pow((x2-x1), 2) + pow((y2-y1), 2));
+    printf("%f\n", distance);
+
+// Return: A double representing the distance
+return distance;
+}
+
 
 int main(int argc, char **argv)
 {
+    
+double askForUserInput() ;
 
-printf("wow new");
-// Output the integer
-int num = 1;
-    printf("\nEntered integer is: %d", num);
-
-// calculateDistance () ;
+calculateDistance () ;
 // calculatePerimeter () ;
 // calculateArea () ;
 // calculateWidth () ;
