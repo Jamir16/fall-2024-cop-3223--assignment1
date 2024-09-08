97cbed6a309741a7af2cfd2e4405b1c522ee71d5
Jamir16 <ja861910@ucf.edu>
2024-09-08 19:21:58 +0000

    setting up main

---
+++ fracturing.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void calculateDistance ( int arg1 , int arg2 )
+{
+printf ("function") ;
+}
+
+int main(int argc, char **argv)
+{
+
+printf("wow new");
+// Output the integer
+int num = 1;
+    printf("\nEntered integer is: %d", num);
+
+// calculateDistance () ;
+// calculatePerimeter () ;
+// calculateArea () ;
+// calculateWidth () ;
+// calculateHeight () ;
+
+return 0;
+}
